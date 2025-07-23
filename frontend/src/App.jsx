dcbd91f07b9f169ec0c102e7572b7205d091a3a4
utkarsh <u.saraswat30@gmail.com>
2025-07-23 22:23:14 +0530

    App.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     setLoading(true);
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/download',
+      const response = await axios.post('https://vidextract-backend.onrender.com/api/download',
         { url },
         { responseType: 'blob' }
       );
